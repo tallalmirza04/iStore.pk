e1048f8b1dc355eeb229384174316661aa46c109
CYGNUS <macbookpro@192.168.1.10>
2026-05-01 15:30:16 +0500

    backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,29 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+
+//     console.log(`🟢 MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error("❌ MongoDB Connection Failed:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+
+
+// deploy on railway error
+
+
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log(`🟢 MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error.message);
@@ -11,4 +31,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
